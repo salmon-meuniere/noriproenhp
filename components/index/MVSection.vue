cee3992d3972e4b6996efad1690c26b3378f5102
salmon-meuniere <daisho7308@gmail.com>
2022-06-01 23:21:26 +0900

    (delete) graduated talent

--- components/index/MVSection.vue
+++ components/index/MVSection.vue
@@ -5,8 +5,18 @@
                 <h2 class="sectionHeader__main">MV</h2>
                 <p class="sectionHeader__sub" lang="en">Music Videos</p>
             </header>
-            <div class="mv__wrap"><iframe class="iframe" src="https://www.youtube.com/embed/hJWI3mIF8rc?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+            <div class="mv__wrap"><iframe class="iframe" :src="'https://www.youtube.com/embed/' + mv[0] + '?controls=0'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
             <div class="more"><nuxt-link class="button-secondary" to="/mv">See more</nuxt-link></div>
         </div>
     </section>
 </template>
+<script>
+import mv from "@/assets/datas/mv.json";
+export default {
+    data() {
+        return {
+            mv
+        }
+    }
+}
+</script>
